f89b7ce5be06ed9b6eab2a763fd95b7193e3c2d1
MrSushi <La1422@Rambler.ru>
2022-11-15 03:50:48 +0700

    отчет о пользователях

--- main.py
+++ main.py
@@ -16,32 +16,28 @@ cursor = conn.cursor()
 bot_greeting = 'Привет я бот Hook Production для уведомлений об оплате налога'
 
 
-def db_table_user(user_id: int):
-    cursor.execute('INSERT OR IGNORE INTO notify_user (user_id) VALUES (?)', (user_id,))
+def db_table_user(user_id: int, username: str):
+    cursor.execute('INSERT OR IGNORE INTO notify_user (user_id, username) VALUES (?, ?);', (user_id, username))
     conn.commit()
 
 
 def db_table_date(date_1=None, date_2=None):
-    exist_date_1 = cursor.execute('SELECT EXISTS(SELECT date_1 FROM notify_date)').fetchone()
-    exist_date_2 = cursor.execute('SELECT EXISTS(SELECT date_2 FROM notify_date)').fetchone()
+    exist_date_1 = cursor.execute('SELECT EXISTS(SELECT date_1 FROM notify_date);').fetchone()
+    exist_date_2 = cursor.execute('SELECT EXISTS(SELECT date_2 FROM notify_date);').fetchone()
     if date_1 and exist_date_1:
         cursor.execute('UPDATE notify_date SET date_1 = ?;', (date_1,))
     elif date_2 and exist_date_2:
         cursor.execute('UPDATE notify_date SET date_2 = ?;', (date_2,))
     else:
-        cursor.execute('INSERT OR IGNORE INTO notify_date (date_1, date_2) VALUES (?, ?)', (date_1, date_2))
+        cursor.execute('INSERT OR IGNORE INTO notify_date (date_1, date_2) VALUES (?, ?);', (date_1, date_2))
     conn.commit()
-
-
-# def db_table_date_2(date_2: int):
-#     cursor.execute('INSERT OR IGNORE INTO notify_date (date_2) VALUES (?)', (date_2,))
-#     conn.commit()
 
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     chat_id = message.chat.id
-    db_table_user(user_id=chat_id)
+    username = message.chat.username
+    db_table_user(user_id=chat_id, username=username, message='Оплатите налог и вышлите чек')
     bot.send_message(chat_id, text=bot_greeting)
 
 
@@ -68,18 +64,16 @@ def second_date(message):
 @bot.message_handler(commands=['get_all_users'])
 def all_users(message):
     text = ''
-    users = cursor.execute('SELECT user_id FROM notify_user').fetchall()
+    users = cursor.execute('SELECT ("ID Пользователя: " || user_id || " Никнейм: @" || username || " Сообщение: " || ifnull(message, "не задано")) FROM notify_user;').fetchall()
     for user in users:
-        text += f'<b>Юзернейм: {user.username}</b>\n' \
-                f'<b>Сообщение {user.message}</b>\n' \
-                f'<b>ID юзера: {user.user_id}</b>\n'
+        text += f'<b>{user}</b>\n'
     bot.send_message(message.chat.id, text=text, parse_mode='HTML')
 
 
 def auto_send_message():
-    users = cursor.execute('SELECT user_id FROM notify_user').fetchall()
-    exist_date_1 = cursor.execute('SELECT date_1 FROM notify_date').fetchone()
-    exist_date_2 = cursor.execute('SELECT date_2 FROM notify_date').fetchone()
+    users = cursor.execute('SELECT user_id FROM notify_user;').fetchall()
+    exist_date_1 = cursor.execute('SELECT date_1 FROM notify_date;').fetchone()
+    exist_date_2 = cursor.execute('SELECT date_2 FROM notify_date;').fetchone()
     today = datetime.now(pytz.utc).day
     if exist_date_1 or exist_date_2:
         for user in users:
@@ -87,10 +81,9 @@ def auto_send_message():
                 bot.send_message(chat_id=user, text='Пожалуйста оплатите налог')
 
 
-# schedule.every().day().at('17:00').do(auto_send_message)
-
-def run_bot():
-    bot.polling(none_stop=True, interval=0)
+# def run_bot():
+#     bot.polling(none_stop=True, interval=0)
+bot.polling(none_stop=True, interval=0)
 
 
 def run_scheduler():
@@ -101,11 +94,11 @@ def run_scheduler():
         sleep(1)
 
 
-if __name__ == '__main__':
-    task_1 = threading.Thread(target=run_bot)
-    task_2 = threading.Thread(target=run_scheduler)
-    task_1.start()
-    task_2.start()
+# if __name__ == '__main__':
+#     task_1 = threading.Thread(target=run_bot)
+#     # task_2 = threading.Thread(target=run_scheduler)
+#     task_1.start()
+#     # task_2.start()
 
 # schedule.every(1).minutes.do(auto_send_message)
 # schedule.every().day.at('18:00').do(send_message)
