74c3d199bfcc5fbc48971e4cccd8e90281a9f360
mrsushi <La1422@Rambler.ru>
2022-11-14 18:15:41 +0700

    отправка по заданным дням

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import pytz
 import sqlite3
 import telebot
 import schedule
 
+from datetime import datetime
 from time import sleep
 
 
@@ -14,27 +16,70 @@ cursor = conn.cursor()
 bot_greeting = 'Привет я бот Hook Production для уведомлений об оплате налога'
 
 
-def db_table_val(user_id: int):
+def db_table_user(user_id: int):
     cursor.execute('INSERT OR IGNORE INTO notify_user (user_id) VALUES (?)', (user_id,))
     conn.commit()
+
+
+def db_table_date(date_1=None, date_2=None):
+    exist_date_1 = cursor.execute('SELECT EXISTS(SELECT date_1 FROM notify_date)').fetchone()
+    exist_date_2 = cursor.execute('SELECT EXISTS(SELECT date_2 FROM notify_date)').fetchone()
+    if date_1 and exist_date_1:
+        cursor.execute('UPDATE notify_date SET date_1 = ?;', (date_1,))
+    elif date_2 and exist_date_2:
+        cursor.execute('UPDATE notify_date SET date_2 = ?;', (date_2,))
+    else:
+        cursor.execute('INSERT OR IGNORE INTO notify_date (date_1, date_2) VALUES (?, ?)', (date_1, date_2))
+    conn.commit()
+
+
+# def db_table_date_2(date_2: int):
+#     cursor.execute('INSERT OR IGNORE INTO notify_date (date_2) VALUES (?)', (date_2,))
+#     conn.commit()
 
 
 @bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     chat_id = message.chat.id
-    db_table_val(user_id=chat_id)
+    db_table_user(user_id=chat_id)
     bot.send_message(chat_id, text=bot_greeting)
+
+
+@bot.message_handler(commands=['change_date_1', 'change_date_2'])
+def change_settings(message):
+    if message.text == '/change_date_1':
+        send_msg = bot.send_message(message.chat.id, text='Введите первую дату')
+        bot.register_next_step_handler(send_msg, first_date)
+    elif message.text == '/change_date_2':
+        send_msg = bot.send_message(message.chat.id, text='Введите вторую дату')
+        bot.register_next_step_handler(send_msg, second_date)
+
+
+def first_date(message):
+    db_table_date(date_1=message.text)
+    bot.send_message(message.chat.id, text='Первая дата изменена')
+
+
+def second_date(message):
+    db_table_date(date_2=message.text)
+    bot.send_message(message.chat.id, text='Вторая дата изменена')
 
 
 def auto_send_message():
     users = cursor.execute('SELECT user_id FROM notify_user').fetchall()
-    for user in users:
-        bot.send_message(chat_id=user, text='Пожалуйста оплатите налог')
+    exist_date_1 = cursor.execute('SELECT date_1 FROM notify_date').fetchone()
+    exist_date_2 = cursor.execute('SELECT date_2 FROM notify_date').fetchone()
+    today = datetime.now(pytz.utc).day
+    if exist_date_1 or exist_date_2:
+        for user in users:
+            if today == exist_date_1 or today == exist_date_2:
+                bot.send_message(chat_id=user, text='Пожалуйста оплатите налог')
 
 
-bot.polling(none_stop=True)
+# bot.polling(none_stop=True)
 
-schedule.every(5).seconds.do(auto_send_message)
+schedule.every(1).seconds.do(auto_send_message)
+# schedule.every().day().do(auto_send_message)
 
 while True:
     schedule.run_pending()
